c52bd065eeb3b0fd0de77132797c59974fe12540
Nafees Iqbal <33998031+iqbalnafee@users.noreply.github.com>
2021-07-06 00:19:18 +0600

    number type useState done

--- src/App.js
+++ src/App.js
@@ -7,15 +7,31 @@ function App() {
 
   const [flag, setFlag] = useState(false);
 
+  const [steps, setSteps] = useState(0);
+
   const changeName = () => {
-    //setName("Iqbal");
     setFlag(!flag);
+  }
+
+  const increment = () => {
+    setSteps(steps + 1);
+  }
+
+  const decrement = () => {
+    setSteps(steps - 1);
   }
 
   return (
     <div className="App">
       <div>Hello {flag ? name : ""}</div>
       <button className="btn-primary" onClick={changeName} style={{ marginTop: "10px" }}> Click Me </button>
+
+      <hr></hr>
+
+      <button className="btn-primary" onClick={increment} style={{ marginTop: "10px" }}> +</button>
+      <div style={{ marginTop: "10px" }}>{steps}</div>
+      <button className="btn-primary" onClick={decrement} style={{ marginTop: "10px" }}> -</button>
+
     </div >
   );
 }
